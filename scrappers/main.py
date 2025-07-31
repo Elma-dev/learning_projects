8977f37c1137b155728ce2d31d47d9d1daf13c68
Elma-dev <67378945+Elma-dev@users.noreply.github.com>
2025-07-31 11:50:56 +0100

    Update main.py

--- scrappers/main.py
+++ scrappers/main.py
@@ -6,11 +6,11 @@ RED = "\033[91m"
 RESET = "\033[0m"
 
 if __name__=="__main__":
-    post_links_path="facebook_post_links.json"
+    post_links_path="fpost_links.json"
     with open(post_links_path,"r") as f:
         post_links = json.loads(f.read())["post_links"]
     facebook_post_scraper= FacebookPostScraper()
-    for i,pl in enumerate(post_links[463:]):
+    for i,pl in enumerate(post_links):
         print(f"{GREEN}[INFO] Scrap Post {i+1} {RESET}")
         result=facebook_post_scraper.scrape_post_and_comments(post_url=pl)
         # {"post_text": self.post_text, "comments": list(set(self.comments))}
