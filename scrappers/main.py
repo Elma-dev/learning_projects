e6d53a7aa953f5b71574a84dfa513c8b47ea6a35
abdeljalil el majjodi <aelmajjodi@abdeljalils-MacBook-Air.local>
2025-07-31 11:50:22 +0100

    add main

--- scrappers/main.py
+++ scrappers/main.py
@@ -10,7 +10,7 @@ if __name__=="__main__":
     with open(post_links_path,"r") as f:
         post_links = json.loads(f.read())["post_links"]
     facebook_post_scraper= FacebookPostScraper()
-    for i,pl in enumerate(post_links):
+    for i,pl in enumerate(post_links[463:]):
         print(f"{GREEN}[INFO] Scrap Post {i+1} {RESET}")
         result=facebook_post_scraper.scrape_post_and_comments(post_url=pl)
         # {"post_text": self.post_text, "comments": list(set(self.comments))}
